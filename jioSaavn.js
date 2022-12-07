865e3b9ecf7ee1316f66da6d0bd1500809e567cd
Ravi <ravidodabhangi123@gmail.com>
2022-12-07 13:47:53 +0530

    jio-savaan-clone

---
+++ jioSaavn.js
@@ -0,0 +1,710 @@
+let Mysongs = [
+  {
+    id: 1,
+    language: "KANNADA",
+    AlbumName: "Ek Love Ya",
+    Artist: "vijay Prakash",
+    image: "./image/2000sRo.jpg",
+    audio: "./audio/Hrudayada Paadu.mp3",
+  },
+  {
+    id: 2,
+    language: "KANNADA",
+    AlbumName: "Ra Ra Rakkamma",
+    Artist: "Nakul Abhyankar",
+    image: "./image/Mehbooba.jpg",
+    audio: "./audio/Eyy Maga Idhu Nan Jagaa.mp3",
+  },
+  {
+    id: 3,
+    language: "KANNADA",
+    AlbumName: "Love Mocktail",
+    Artist: "vijay Prakash",
+    image: "./image/Love-Mocktail-2.jpg",
+    audio: "./audio/Hey Nalle Nalle.mp3",
+  },
+  {
+    id: 4,
+    language: "KANNADA",
+    AlbumName: "Arabic Kuttu",
+    Artist: "Anirudh",
+    image: "./image/Har-har-Shambhu-mahadeva.jpg",
+    audio: "./audio/Har Har Shambhu.mp3",
+  },
+  {
+    id: 5,
+    language: "KANNADA",
+    AlbumName: "Trademark",
+    Artist: "Nakul Abhyankar",
+    image: "./image/Hey-Fakira.jpg",
+    audio: "./audio/Bombe Helutaite.mp3",
+  },
+  {
+    id: 6,
+    language: "KANNADA",
+    AlbumName: "Kgf-Chapter-2",
+    Artist: "vijay Prakash",
+    image: "./image/Kgf-Chapter.jpg",
+    audio: "./audio/Mehabooba.mp3",
+  },
+  {
+    id: 7,
+    language: "KANNADA",
+    AlbumName: "love 360",
+    Artist: "Nakul Abhyankar",
+    image: "./image/Love-360.jpg",
+    audio: "./audio/Hrudayada Paadu.mp3",
+  },
+  {
+    id: 8,
+    language: "KANNADA",
+    AlbumName: "Hey Fakira",
+    Artist: "Shreya Ghoshal",
+    image: "./image/Hejje-Hejjeg.jpg",
+    audio: "./audio/Jotheyali.mp3",
+  },
+  {
+    id: 9,
+    language: "KANNADA",
+    AlbumName: "Mugilpete",
+    Artist: "sonu nigam",
+    image: "./image/Mugilpet.jpg",
+    audio: "./audio/Barisu KANNADA Dindima.mp3",
+  },
+  {
+    id: 10,
+    language: "KANNADA",
+    AlbumName: "Thothapuri",
+    Artist: "Shreya Ghoshal",
+    image: "./image/Naa-Ninage-Kaavalugaara.jpg",
+    audio: "./audio/Ninna Jothe Nanna Kathe.mp3",
+  },
+  {
+    id: 11,
+    language: "HINDI",
+    AlbumName: "Baglu Tegi Meri Jaan",
+    Artist: "Arijit Singh ",
+    image: "./image/AllArijit.jpg",
+    audio: "./audio/Pyaar Karte Ho Na.mp3",
+  },
+  {
+    id: 12,
+    language: "HINDI",
+    AlbumName: "beast mode",
+    Artist: "Anirudh ",
+    image: "./image/Chalo-Theek-Hai.jpg",
+    audio: "./audio/Raatan Lambiyan.mp3",
+  },
+  {
+    id: 13,
+    language: "HINDI",
+    AlbumName: "chadda",
+    Artist: "tippu ",
+    image: "./image/DesiR.jpg",
+    audio: "./audio/Saami Saami.mp3",
+  },
+  {
+    id: 14,
+    language: "HINDI",
+    AlbumName: "Shamshera",
+    Artist: "Arijit Singh ",
+    image: "./image/Dil-From-Ek.jpg",
+    audio: "./audio/Raatan Lambiyan.mp3",
+  },
+  {
+    id: 15,
+    language: "HINDI",
+    AlbumName: "Kuch Baatein",
+    Artist: " Sumit Bhalla",
+    image: "./image/SonuNigamA.jpg",
+    audio: "./audio/Galliyan Returns.mp3",
+  },
+  {
+    id: 16,
+    language: "HINDI",
+    AlbumName: "Padmaavat",
+    Artist: "Bhansali ",
+    image: "./image/Tauba-feat.jpg",
+    audio: "./audio/Meri Zindagi Hai Tu.mp3",
+  },
+  {
+    id: 17,
+    language: "HINDI",
+    AlbumName: "Lsingh",
+    Artist: "Arih ",
+    image: "./image/Dil-From-Ek.jpg",
+    audio: "./audio/Ami Je Tomar.mp3",
+  },
+  {
+    id: 18,
+    language: "HINDI",
+    AlbumName: "Arabic Kuttu",
+    Artist: "Anirudh ",
+    image: "./image/Ijazzat-Hai.jpg",
+    audio: "./audio/Meri Zindagi Hai Tu.mp3",
+  },
+  {
+    id: 19,
+    language: "HINDI",
+    AlbumName: "Gone case",
+    Artist: "Arijit Singh ",
+    image: "./image/Iss-Baarish-Mei.jpg",
+    audio: "./audio/Galliyan Returns.mp3",
+  },
+  {
+    id: 20,
+    language: "HINDI",
+    AlbumName: "Mangalyam",
+    Artist: " Sumit Bhalla",
+    image: "./image/Iss-Baarish-Mei.jpg",
+    audio: "./audio/Tumse Pyaar Karke.mp3",
+  },
+  {
+    id: 21,
+    language: "ENGLISH",
+    AlbumName: "Light Switch",
+    Artist: "Charlie Puth",
+    image: "./image/Light-Switch.jpg",
+    audio: "./audio/Someone_To_You.mp3",
+  },
+  {
+    id: 22,
+    language: "ENGLISH",
+    AlbumName: "Heat Waves",
+    Artist: "Harry Styles ",
+    image: "./image/F-CK-LOVE-3-OVER.jpg",
+    audio: "./audio/Running_With_The_Wolves.mp3",
+  },
+  {
+    id: 23,
+    language: "ENGLISH",
+    AlbumName: "Heartbeats",
+    Artist: " Lizzo",
+    image: "./image/You-ENGLISH.jpg",
+    audio: "./audio/Already_Gone.mp3",
+  },
+  {
+    id: 24,
+    language: "ENGLISH",
+    AlbumName: "home",
+    Artist: "Harry Styles ",
+    image: "./image/You-ENGLISH.jpg",
+    audio: "./audio/Home.mp3",
+  },
+  {
+    id: 25,
+    language: "ENGLISH",
+    AlbumName: "memories",
+    Artist: "Kid Laroi",
+    image: "./image/MostSearched.jpg",
+    audio: "./audio/Memories.mp3",
+  },
+  {
+    id: 26,
+    language: "ENGLISH",
+    AlbumName: "runaway",
+    Artist: "Justin Bieber",
+    image: "./image/Voodoo.jpg",
+    audio: "./audio/Runaway.mp3",
+  },
+  {
+    id: 27,
+    language: "ENGLISH",
+    AlbumName: "hold on",
+    Artist: "Charlie Puth",
+    image: "./image/TreadmillPop.jpg",
+    audio: "./audio/Hold_On.mp3",
+  },
+  {
+    id: 28,
+    language: "ENGLISH",
+    AlbumName: "this is me",
+    Artist: "Lizzo",
+    image: "./image/Heat-Waves.jpg",
+    audio: "./audio/This is me.mp3",
+  },
+  {
+    id: 29,
+    language: "ENGLISH",
+    AlbumName: "snowman",
+    Artist: "Justin Bieber",
+    image: "./image/Heartbeats.jpg",
+    audio: "./audio/Snowman.mp3",
+  },
+  {
+    id: 30,
+    language: "ENGLISH",
+    AlbumName: "unstoppable",
+    Artist: "Charlie Puth",
+    image: "./image/Bones.jpg",
+    audio: "./audio/Unstoppable.mp3",
+  },
+];
+
+// Fetching data from Array of Object
+
+let audioContent = document.querySelector(".audio_content");
+let searchBox = document.querySelector("input");
+let displayAlbum = document.querySelector(".displayAlbums");
+let displaysongs = document.querySelector(".displaySongs");
+
+// function loading(){
+  for (let value of Mysongs) {
+    let { image, AlbumName, Artist, audio, id } = value;
+  
+    audioContent.innerHTML += `<aside class="audio_gridData">
+        <div>
+          <figure>
+          <img class="arr_img" data_type="${Artist}" no="${id}" src="${image}" alt="${AlbumName}">
+          <i da="${id}" class="fa-solid fa-circle-play fpicon"></i>
+            <audio src="${audio}"></audio>
+          </figure>
+          <p class="ap">${AlbumName}</p>
+          <p class="arp">${Artist}</p>   
+        </div>
+      </aside>`;
+  }
+// }
+
+// document.addEventListener("DOMContentLoaded",e=>{
+//   loading();
+  
+// });
+
+
+  let Search = document.querySelector("#inputSearch");
+
+  Search.addEventListener(("keyup"),e=>{
+    if(e.key==="Enter"&& e.target.value!==" ")
+    {
+      audioContent.innerHTML="";
+      searchMethod(e.target.value);
+      
+    }
+  
+})
+function searchMethod(inputValue){
+let filterArray=
+Mysongs.filter((val)=>{
+if(val.AlbumName==inputValue||val.Artist==inputValue||val.language==inputValue)
+{
+ return val;
+}
+})
+let output1=""
+
+filterArray.forEach((val)=>{
+audioContent.innerHTML+=`<div class="data-container">
+<img src="${val.image}" class="image"></img>
+
+<audio src="${val.audio}" type="audio/mp3"></audio>
+<p>${val.AlbumName}</p>
+</div>`
+
+})
+
+}
+
+
+//todo----first page btn change code start=---------------
+let fpicon = document.querySelectorAll(".fpicon");
+fpicon.forEach((btns) => {
+  let starter = false;
+  let player = () => {
+    starter = true;
+    btns.classList.replace("fa-circle-play", "fa-circle-pause");
+  };
+  let pauser = () => {
+    starter = false;
+    btns.classList.replace("fa-circle-pause", "fa-circle-play");
+  };
+  btns.addEventListener("click", () => {
+    if (starter == false) {
+      player();
+    } else if (starter == true) {
+      pauser();
+    }
+  });
+});
+//todo----first page btn change code end=---------------
+
+//todo=======Data fetch from first page code start-------------------
+
+let model_section = document.querySelector("#model_section");
+let img_cluster = document.querySelectorAll(".audio_gridData div figure img");
+let album_text = document.querySelector(".audio_main .main_model_div1 .har");
+let artist_text = document.querySelector(".audio_main .main_model_div2 p");
+let no_span = document.querySelector(".no_span");
+img_cluster.forEach((img) => {
+  img.addEventListener("click", (e) => {
+    model_section.style.display = "block";
+    document.body.style.overflow = "hidden";
+    document.querySelector(
+      "#model_section article .model_img_aside figure img"
+    ).src = img.getAttribute("src");
+    document.querySelector(
+      "#model_section article .containt_aside .containt_div1 h1"
+    ).textContent = img.getAttribute("alt");
+    document.querySelector(
+      "#model_section article .containt_aside .containt_div1 .span1"
+    ).textContent = img.getAttribute("data_type");
+    document.querySelector(
+      "#model_section article .containt_aside .containt_div1 .span2"
+    ).textContent = img.getAttribute("data_type");
+    album_text.textContent = img.getAttribute("alt");
+    artist_text.textContent = img.getAttribute("data_type");
+    no_span.textContent = img.getAttribute("no");
+  });
+});
+
+//todo=======Data fetch from first page code end-------------------
+
+//todo========Model code with local storage code start--------------
+
+let icon_change = document.querySelector(".audio_main");
+let favorite = document.querySelector(".favorite");
+let play_btnn = document.querySelector("#play_btnn");
+
+icon_change.addEventListener("mouseenter", (e) => {
+  let child = icon_change.children[0].children[0];
+  child.innerHTML = `<i class="fa-solid fa-circle-play"></i>`;
+  child.style.fontSize = "20px";
+});
+icon_change.addEventListener("mouseleave", (e) => {
+  let child = icon_change.children[0].children[0];
+  child.innerHTML = `1`;
+  child.style.fontSize = "16px";
+});
+
+let heart_anchor = document.querySelector(".heart_anchor");
+let likes = document.querySelector(".favorite");
+let data = localStorage.getItem("count");
+
+counter = localStorage.getItem("counter");
+likes.innerHTML = counter;
+heart_anchor.addEventListener("click", (e) => {
+  counter++;
+  likes.innerHTML = counter;
+  localStorage.setItem("counter", counter);
+});
+
+heart_anchor.addEventListener("mousedown", (e) => {
+  heart_anchor.style.color = "red";
+  heart_anchor.style.borderColor = "red";
+});
+heart_anchor.addEventListener("mouseup", (e) => {
+  heart_anchor.style.color = "gray";
+  heart_anchor.style.borderColor = "gray";
+});
+
+//todo========Model code with local storage code End--------------
+
+//todo====Audio footer data start---------------------------------
+
+let audio_src = document.querySelector(".audio_src");
+let fa_play = document.querySelector(".fa-play");
+let backward_btn = document.querySelector(".fa-backward-step");
+let forward_btn = document.querySelector(".fa-forward-step");
+let fa_repeat = document.querySelector(".fa-repeat");
+let audio_img = document.querySelector(".audio_img");
+let album_name = document.querySelector(".album_name");
+let artist_name = document.querySelector(".artist_name");
+let shuffle = document.querySelector(".fa-shuffle");
+let duration = document.querySelector(".duration");
+let song_time = document.querySelector(".song_time");
+let currentTime = document.querySelector(".currentTime");
+let rewinder = document.querySelector(".fa-rotate-left");
+let fast_back = document.querySelector(".fa-backward-fast");
+let fast_forward = document.querySelector(".fa-forward-fast");
+let track_path = document.querySelector(".tracker_div");
+let duration_p = document.querySelector(".duration_p");
+let button_fp = document.querySelectorAll(".fpicon");
+
+//todo=====model play button code start=-----------
+
+let changer = 0;
+play_btnn.addEventListener("click", () => {
+  changer = no_span.textContent - 1;
+  back_forward(Mysongs);
+  audio_src.play();
+  fa_play.classList.replace("fa-play", "fa-pause");
+});
+
+//todo=====model play button code end=-----------
+
+//todo=====first page play button code star=-----------
+
+button_fp.forEach((butn) => {
+  let audio_data = false;
+  let audio_py = () => {
+    audio_data = true;
+    changer = butn.getAttribute("da") - 1;
+    back_forward(Mysongs);
+    audio_src.play();
+    fa_play.classList.replace("fa-play", "fa-pause");
+  };
+  let audio_pe = () => {
+    audio_data = false;
+    changer = butn.getAttribute("da") - 1;
+    back_forward(Mysongs);
+    audio_src.pause();
+    fa_play.classList.replace("fa-pause", "fa-play");
+  };
+  butn.addEventListener("click", () => {
+    if (audio_data == false) {
+      audio_py();
+    } else if (audio_data == true) {
+      audio_pe();
+    }
+  });
+});
+
+//todo=====first page play button code end=-----------
+
+//todo=====footer play and pause btn code sart-------------
+
+let loader = false;
+let audio_play_btn = () => {
+  loader = true;
+  audio_src.play();
+  fa_play.classList.replace("fa-play", "fa-pause");
+};
+let audio_pause_btn = () => {
+  loader = false;
+  audio_src.pause();
+  fa_play.classList.replace("fa-pause", "fa-play");
+};
+
+fa_play.addEventListener("click", (e) => {
+  if (loader) {
+    audio_pause_btn();
+  } else {
+    audio_play_btn();
+  }
+});
+
+//todo=====footer play and pause btn code end-------------
+
+//todo======array fetch and display in footer code start-----------
+
+let back_forward = (Mysongs) => {
+  let song_changer = Mysongs[changer];
+  audio_img.src = song_changer.image;
+  album_name.textContent = song_changer.AlbumName;
+  artist_name.textContent = song_changer.Artist;
+  audio_src.src = song_changer.audio;
+};
+back_forward(Mysongs);
+
+//todo======array fetch and display in footer code end-----------
+
+//todo=====forward and backward btn code start---------
+
+let nextSong = () => {
+  if (changer === Mysongs.length - 1) {
+    changer = 0;
+  }
+  changer++;
+  back_forward(Mysongs);
+  if (fa_play.classList.contains("fa-pause")) {
+    audio_src.play();
+  } else {
+    audio_src.pause();
+  }
+};
+forward_btn.addEventListener("click", nextSong);
+
+backward_btn.addEventListener("click", () => {
+  if (changer == 0) {
+    changer = Mysongs.length;
+  }
+  changer--;
+  back_forward(Mysongs);
+  if (fa_play.classList.contains("fa-pause")) {
+    audio_src.play();
+  } else {
+    audio_src.pause();
+  }
+});
+
+//todo=====forward and backward btn code end---------
+
+//todo=====shuffle btn code start---------
+
+shuffle.addEventListener("click", () => {
+  let song_shuffle1 = Math.ceil(Math.random() * 10);
+  let song_shuffle2 = Math.ceil(Math.random() * 100);
+  if (song_shuffle2 <= 30) {
+    changer = song_shuffle2;
+  } else {
+    changer = song_shuffle1;
+  }
+  back_forward(Mysongs);
+  if (fa_play.classList.contains("fa-pause")) {
+    audio_src.play();
+  } else {
+    audio_src.pause();
+  }
+});
+
+//todo=====forward and backward btn code end---------
+
+//todo=====audio time in browser code start---------
+
+let song_min;
+let song_sec;
+let curr_min;
+let curr_sec;
+audio_src.addEventListener("timeupdate", (e) => {
+  song_min = Math.floor(e.target.duration / 60);
+  song_sec = Math.floor(e.target.duration % 60);
+  curr_min = Math.floor(e.target.currentTime / 60);
+  curr_sec = Math.floor(e.target.currentTime % 60);
+  let track_path_value = (e.target.currentTime / e.target.duration) * 100;
+  track_path.style.width = `${track_path_value}%`;
+  if (e.target.duration) {
+    duration.innerHTML = `${song_min}:${song_sec}`;
+    currentTime.innerHTML = `${curr_min}:${curr_sec}`;
+    duration_p.innerHTML = `${song_min}:${song_sec}`;
+  }
+});
+
+//todo=====audio time in browser code end---------
+
+//todo======audio loop code start---------
+let repeater = false;
+if (repeater === false) {
+  audio_src.addEventListener("ended", nextSong);
+}
+
+let simple_repeat = () => {
+  repeater = true;
+  fa_repeat.style.color = "blue";
+  audio_src.loop = true;
+  audio_src.load();
+};
+let loop_repeat = () => {
+  repeater = false;
+  fa_repeat.style.color = "rgba(0, 0, 0, 0.756)";
+  audio_src.loop = false;
+  audio_src.load();
+};
+
+fa_repeat.addEventListener("click", () => {
+  if (repeater == false) {
+    simple_repeat();
+  } else if (repeater == true) {
+    loop_repeat();
+  }
+});
+//todo======audio loop code end---------
+
+//todo fast forward and backward code start-------------
+
+rewinder.addEventListener("click", () => {
+  audio_src.currentTime = 0;
+});
+fast_back.addEventListener("click", () => {
+  audio_src.currentTime -= 5;
+});
+fast_forward.addEventListener("click", () => {
+  audio_src.currentTime += 5;
+});
+
+//todo fast forward and backward code end-------------
+
+//todo====Audio footer data end---------------------------------
+
+
+//Juley Code
+//dropDown functionality
+
+let arrow=document.querySelector(".arrow");
+// console.log(arrow);
+let DL=document.querySelector(".dropdownList")
+// console.log(DL);
+ arrow.children[0].addEventListener("click",e=>{
+      //  console.log("hello");
+       DL.style.visibility="visible";
+      arrow.children[0].style.visibility="hidden";
+      arrow.children[1].style.visibility="visible";
+ })
+ arrow.children[1].addEventListener("click",e=>{
+  // console.log("hello");
+  DL.style.visibility="hidden";
+  arrow.children[1].style.visibility="hidden";
+  arrow.children[0].style.visibility="visible";
+
+})
+
+
+// functionality of para
+let lang=document.querySelector("#lang");
+// console.log(lang);
+let ul=document.querySelector("#bhasa");
+// console.log(ul);
+ul.addEventListener(("click"),e=>{
+   
+  //  console.log(e.target.innerText);
+    lang.innerText+=`${e.target.innerText},`;
+})
+
+
+//language Search
+ let langSongs="";
+let upgrade=document.querySelector("#upgrade");
+// console.log(upgrade);
+upgrade.addEventListener(("click"),e=>{
+  audioContent.innerHTML="";
+  let langArr=lang.innerText.split(",");
+  langArr.pop();
+  langArr.forEach((val)=>{
+    
+      searchMethod(val.toUpperCase());
+                console.log(val);
+          
+})
+});
+
+// let menuList=document.querySelector(".menu_list")
+// // console.log(menuList);
+// menuList.addEventListener("click",e=>{//   audioContent.innerHTML="";
+//   console.log(e.target.innerText);
+//   if(e.target.innerText=="Home")
+//   {
+//     loading();
+//   }
+// })
+
+
+
+//   // Filtering the data and display on the webpage
+//   // searchBox.addEventListener("keyup", (e) => {
+//   //   if (e.target.value !== " " && e.key === "Enter") {
+//   //     audioContent.innerHTML = " ";
+//   //     let arr = Mysongs.filter((ele) => {
+//   //       return ele.AlbumName === e.target.value;
+//   //     });
+//   //     arr.forEach((ele) => {
+//   //       let { AlbumName, Artist, image, audio, id } = ele;
+//   //       audioContent.innerHTML += `<aside class="filteredAudio">
+//   //               <div>
+//   //                   <figure>
+//   //                       <img class="arr_img" data_type="${Artist}" no="${id}" src="${image}" alt="${AlbumName}">
+//   //   function                </figure>
+//   //                   <div>
+//   //                   <p class="ap">${AlbumName}</p>
+//   //               <p class="arp">${Artist}</p>
+//   //               </div>
+//   //               </div>
+//   //           </aside>`;
+//   //     });
+//   //   }
+//   // });
+
+//   //Search Box
+
+
+
+
+
